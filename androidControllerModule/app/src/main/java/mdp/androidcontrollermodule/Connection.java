13b507bfbf09a53b269c8a3602726f31c6d7ef24
shelinals97 <shelinals97@gmail.com>
2017-08-29 10:46:00 +0800

    First Hello World Commit

---
+++ androidControllerModule/app/src/main/java/mdp/androidcontrollermodule/Connection.java
@@ -0,0 +1,13 @@
+package mdp.androidcontrollermodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Connection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+    }
+}
